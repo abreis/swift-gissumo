e09cc315df4e7b95f5f6d53c1b26f82ef10534fd
Andre Braga Reis <andrebragareis@gmail.com>
2016-06-14 15:27:37 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,1 @@
+swiftc -emit-executable src/main.swift -o swift-gissumo
---
+++ src/main.swift
@@ -0,0 +1,8 @@
+/* Andre Braga Reis, 2016
+ * Licensing information can be found in the accompanying LICENSE file.
+ */
+
+import Foundation
+
+print("Hello, World!")
+
