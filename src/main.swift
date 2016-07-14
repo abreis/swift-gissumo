1bf08f571a2c0d249fd622d91487bbd7b2c552de
Andre Braga Reis <andrebragareis@gmail.com>
2016-07-14 18:55:43 +0100

    Read configuration from a supplied Property List file
    Simulator configuration is expected to be on a .plist file, unlike
    past simulators that relied on command-line options.
    Also includes a very barebones .plist configuration.

--- src/main.swift
+++ src/main.swift
@@ -4,5 +4,55 @@
 
 import Foundation
 
-print("Hello, World!")
+/* Process command line options 
+ */
+if Process.arguments.count != 2 || !Process.arguments[1].hasSuffix(".plist") {
+	print("ERROR: Please supply a .plist configuration file.")
+	exit(EXIT_FAILURE)
+}
 
+
+/* Load and validate configuration file
+ */
+print("Reading configuration file...", terminator: "")
+
+let configFileURL = NSURL.fileURLWithPath(Process.arguments[1])
+var configFileError : NSError?
+if !configFileURL.checkResourceIsReachableAndReturnError(&configFileError) {
+	print(" failed\n", configFileError)
+	exit(EXIT_FAILURE)
+}
+
+// Load plist into a configuration dictionary array
+guard let config = NSDictionary(contentsOfURL: configFileURL)
+ else {
+	print(" failed\n", "Error: Invalid configuration file format.")
+	exit(EXIT_FAILURE)
+}
+print(" okay")
+
+
+/* Load floating car data from an XML file
+ * - Check for floatingCarDataFile config entry
+ * - See if file exists
+ * - Parse XML
+ */
+print("Loading floating car data...", terminator: "")
+if let fcdFile = config["floatingCarDataFile"] as? String {
+	// See if the file exists
+	let fcdFileURL = NSURL.fileURLWithPath(fcdFile)
+	var fcdFileError : NSError?
+	if !fcdFileURL.checkResourceIsReachableAndReturnError(&fcdFileError) {
+		print(" failed\n", fcdFileError)
+		exit(EXIT_FAILURE)
+	}
+	
+	print(" todo")
+
+} else {
+	print(" failed\n", "Error: Please specify a SUMO FCD output file with 'floatingCarDataFile'.")
+}
+
+
+
+exit(EXIT_SUCCESS)
