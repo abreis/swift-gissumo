71e39982c56fa2abc92c7a6cbbe7b8c1eb6b079d
Andre Braga Reis <andrebragareis@gmail.com>
2016-07-19 15:37:47 +0100

    Discard OSM roads where passenger vehicles are not allowed

--- scripts/01fetchCityMap/00portoMap.sh
+++ scripts/01fetchCityMap/00portoMap.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-WORKDIR=/data
-
 # This script pulls a square map from OpenStreetMap,
 # and converts it into a SUMO-compatible format.
-export SUMO_HOME=$( find /root/ -type d -name 'sumo-*' -print -quit )
-if [ ! -d "$SUMO_HOME" ]; then echo "SUMO home folder not found."; exit 1; fi
-
-TYPEMAPS=$SUMO_HOME/data/typemap
-if [ ! -d "$TYPEMAPS" ]; then echo "SUMO typemaps folder not found."; exit 1; fi
+WORKDIR=/data
 
 # Porto Map #02 -- 1 sq.km. -- (41.1679,-8.6227),(41.1598,-8.6094)
 LONMIN='-8.6227'
@@ -15,23 +9,36 @@ LONMAX='-8.6094'
 LATMIN='41.1598'
 LATMAX='41.1679'
 
+# Locate SUMO home and typemaps folder
+export SUMO_HOME=$( find /root/ -type d -name 'sumo-*' -print -quit)
+if [ ! -d "$SUMO_HOME" ]; then echo "SUMO home folder not found." ; exit 1; fi
+TYPEMAPS=$SUMO_HOME/data/typemap
+if [ ! -d "$TYPEMAPS" ]; then echo "SUMO typemaps folder not found."; exit 1; fi
+
+OSMFILE=${WORKDIR}/map.osm.xml
+NETFILE=${WORKDIR}/map.net.xml
+BASETYPEMAP=${TYPEMAPS}/osmNetconvert.typ.xml
+URBANTYPEMAP=${TYPEMAPS}/osmNetconvertUrbanDe.typ.xml
+
 # Fetch a map from OSM
-curl -L -o ${WORKDIR}/map.osm.xml "http://overpass-api.de/api/map?bbox=${LONMIN},${LATMIN},${LONMAX},${LATMAX}"
+curl -L -o ${OSMFILE} "http://overpass-api.de/api/map?bbox=${LONMIN},${LATMIN},${LONMAX},${LATMAX}"
 
 # Assert XML validity
-xmllint --noout ${WORKDIR}/map.osm.xml || { echo "Got an invalid XML file from OSM."; exit 1; }
+xmllint --noout ${OSMFILE} || { echo "Got an invalid XML file from OSM."; exit 1; }
 
 # Convert the OSM map to a SUMO road network map
-netconvert --osm-files ${WORKDIR}/map.osm.xml -o ${WORKDIR}/map.net.xml \
-	--type-files ${TYPEMAPS}/osmNetconvert.typ.xml,${TYPEMAPS}/osmNetconvertUrbanDe.typ.xml \
-	--geometry.remove \
-	--remove-edges.isolated \
-	--roundabouts.guess \
-	--ramps.guess \
-	--junctions.join \
-	--tls.guess-signals \
-	--tls.discard-simple \
-	--tls.join
+netconvert --osm-files ${OSMFILE} \
+ --output-file ${NETFILE} \
+ --type-files ${BASETYPEMAP},${URBANTYPEMAP} \
+ --geometry.remove \
+ --remove-edges.isolated \
+ --roundabouts.guess \
+ --ramps.guess \
+ --junctions.join \
+ --tls.guess-signals \
+ --tls.discard-simple \
+ --tls.join \
+ --keep-edges.by-vclass passenger
 
 if [ $? -ne 0 ]; then echo "Map conversion failed."; exit 1; fi
 
