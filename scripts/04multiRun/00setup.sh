00e7b3627786154c22fe07cfdc6732f6e584c40b
Andre Braga Reis <andrebragareis@gmail.com>
2016-09-15 23:05:46 +0100

    Scripts to automate large numbers of simulations with different FCD

---
+++ scripts/04multiRun/00setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+# Parameters
+OSMFILE=/work/map.osm.xml
+STOPTIME=1200
+PERIOD=2
+INITIALSEED=31337
+ROUNDS=100
+
+# Delete SETUPCOMPLETE to enable the script and regenerate data
+if [ -f SETUPCOMPLETE ]; then exit 0; fi
+
+# OSM map must be provided
+if [ ! -f ${OSMFILE} ]; then echo "Missing OSM file."; exit 1; fi
+
+# Copy scripts folder
+[ ! -d mobility ] || rm -rf mobility
+cp -r ../mobility .
+
+# Set SUMO_HOME
+export SUMO_HOME=$( find /root/ -type d -name 'sumo-*' -print -quit )
+if [ ! -d "$SUMO_HOME" ]; then echo "SUMO home folder not found." ; exit 1; fi
+
+# Generate 100 Floating Car Data files
+printf "Generating... "
+for COUNT in $(seq 1 ${ROUNDS}); do
+	printf "${COUNT} "
+	SEED=$(expr $INITIALSEED + $COUNT)
+	./mobility/osm5fcd.sh $OSMFILE $STOPTIME $PERIOD $SEED
+done
+echo "done"
+
+[ -d fcddata ] || mkdir -p fcddata 
+mv mobilitydata/*.fcd.xml fcddata/
+
+touch SETUPCOMPLETE
---
+++ scripts/04multiRun/01simulate.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+
+SIMDIR=simulations
+FCDDIR=fcddata
+
+if [ ! -d ${FCDDIR} ]; then
+	echo "No Floating Car Data."
+	exit 1
+fi
+
+if [ -d ${SIMDIR} ]; then
+	echo "Folder with previous simulations exists, move it before proceeding."
+	exit 1
+fi
+
+if [ ! -f config.plist ]; then
+	echo "Please provide a reference configuration file."
+	exit 1
+fi
+
+if [ ! -f obstructionMask.payload ]; then
+	echo "Please generate and provide an obstruction mask file."
+	exit 1
+fi
+
+# Pull the latest binary
+cp -f ../../build/gissumo_fast .
+
+# Pull the config editor
+cp -f ../../tools/configPlistEditor/build/configPlistEditor .
+
+# Run Swift-GISSUMO on each Floating Car Data file
+mkdir ${SIMDIR}
+FCDCOUNT=$(find ${FCDDIR} -type f -iname '*.fcd.xml' | wc -l)
+SIMSREMAINING=${FCDCOUNT}
+for FCDFILE in $(find ${FCDDIR} -type f -iname '*.fcd.xml'); do
+	SIMULATION=$(basename ${FCDFILE} .fcd.xml)
+	mkdir ${SIMDIR}/${SIMULATION}
+
+	# Copy the reference configuration file over
+	CONFIGFILE=${SIMDIR}/${SIMULATION}/${SIMULATION}.config.plist
+	cp config.plist ${CONFIGFILE}
+
+	# Edit 'floatingCarDataFile' and 'statsFolder' on the .plist 
+	./configPlistEditor ${CONFIGFILE} floatingCarDataFile ${FCDFILE}
+	./configPlistEditor ${CONFIGFILE} stats.statsFolder ${SIMDIR}/${SIMULATION}/stats
+
+	printf "${SIMULATION} ... "
+	SECONDS=0
+
+	./gissumo_fast ${CONFIGFILE} > ${SIMDIR}/${SIMULATION}/gissumo.log 2>&1
+
+	# Run a moving average where each new simulation time is 10% of the new average
+	SIMULATIONTIME=${SECONDS}
+	if [ -z ${PREVSIMTIME} ]; then PREVSIMTIME=${SIMULATIONTIME}; fi
+	SIMTIMEAVG=$(expr \( ${PREVSIMTIME} \* 90 + ${SIMULATIONTIME} \* 10 \) / 100)
+	SIMSREMAINING=$(expr $SIMSREMAINING - 1)
+	ESTTIMEREMAINING=$(expr ${SIMTIMEAVG} \* ${SIMSREMAINING})
+	PREVSIMTIME=${SIMTIMEAVG}
+
+	if [ $SIMSREMAINING -eq 0 ]; then printf "okay\n"; exit 0; fi
+
+	printf "okay, time %ds, %d to go, ETA %dh%02dm%02ds\n" "${SIMULATIONTIME}" "${SIMSREMAINING}" =$((${ESTTIMEREMAINING}/3600)) $((${ESTTIMEREMAINING}%3600/60)) $((${ESTTIMEREMAINING}%60))
+done
