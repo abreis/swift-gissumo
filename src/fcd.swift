ac37ea105c212982baf2a89739e16bfa8e9d8d40
Andre Braga Reis <andrebragareis@gmail.com>
2017-02-21 17:42:45 +0000

    Deprecate reading FCD from XML files

--- src/fcd.swift
+++ src/fcd.swift
@@ -49,57 +49,57 @@ enum FloatingCarDataError: Error, CustomStringConvertible{
  * so we memmap the file and process the XML lazily. This routine should be
  * near-instantaneous.
  */
-func loadFloatingCarData(fromFile fcdFile: String) throws -> XMLIndexer {
-	// See if the file exists
-	let fcdFileURL = URL(fileURLWithPath: fcdFile)
-	guard (fcdFileURL as NSURL).checkResourceIsReachableAndReturnError(nil) else {
-		throw FloatingCarDataError.fileError
-	}
-
-	// Parse XML Floating Car Data
-	guard let fcdData = try? Data(contentsOf: fcdFileURL, options: [.mappedIfSafe, .uncached] ) else {
-		throw FloatingCarDataError.unableToParse
-	}
-
-	// Create an XML indexer for the FCD data
-	let fcdXML = SWXMLHash.config {
-		config in
-		config.shouldProcessLazily = true
-		}.parse(fcdData)
-
-	return fcdXML
-}
+//func loadFloatingCarData(fromFile fcdFile: String) throws -> XMLIndexer {
+//	// See if the file exists
+//	let fcdFileURL = URL(fileURLWithPath: fcdFile)
+//	guard (fcdFileURL as NSURL).checkResourceIsReachableAndReturnError(nil) else {
+//		throw FloatingCarDataError.fileError
+//	}
+//
+//	// Parse XML Floating Car Data
+//	guard let fcdData = try? Data(contentsOf: fcdFileURL, options: [.mappedIfSafe, .uncached] ) else {
+//		throw FloatingCarDataError.unableToParse
+//	}
+//
+//	// Create an XML indexer for the FCD data
+//	let fcdXML = SWXMLHash.config {
+//		config in
+//		config.shouldProcessLazily = true
+//		}.parse(fcdData)
+//
+//	return fcdXML
+//}
 
 // Parse the Floating Car Data into an FCDTimestep array (DEPRECATED)
-func parseFloatingCarData(fromXML fcdXML: XMLIndexer, stopTime configStopTime: Double) throws -> [FCDTimestep] {
-	// Load data onto our Trips array
-	var fcdTrips = [FCDTimestep]()
-	timestepLoop: for timestep in fcdXML["fcd-export"]["timestep"] {
-		guard let timestepElement = timestep.element,
-			let s_time = timestepElement.attribute(by: "time")?.text,
-			let timestepTime = Double(s_time)
-			else {
-				throw FloatingCarDataError.invalidTimestamp
-		}
-
-		// Don't load timesteps that occur later than the simulation stopTime
-		if timestepTime > configStopTime { break timestepLoop }
-
-		var timestepVehicles = [FCDVehicle]()
-		for vehicle in timestep["vehicle"] {
-			guard let vehicleElement = vehicle.element,
-				let s_id = vehicleElement.attribute(by: "id")?.text,
-				let s_xgeo = vehicleElement.attribute(by: "x")?.text,
-				let s_ygeo = vehicleElement.attribute(by: "y")?.text,
-				let v_id = UInt(s_id),
-				let v_xgeo = Double(s_xgeo),
-				let v_ygeo = Double(s_ygeo)
-				else {
-					throw FloatingCarDataError.failedConversion
-			}
-			timestepVehicles.append( FCDVehicle(id: v_id, geo: (x: v_xgeo, y: v_ygeo)) )
-		}
-		fcdTrips.append( FCDTimestep(time: timestepTime, vehicles: timestepVehicles) )
-	}
-	return fcdTrips
-}
+//func parseFloatingCarData(fromXML fcdXML: XMLIndexer, stopTime configStopTime: Double) throws -> [FCDTimestep] {
+//	// Load data onto our Trips array
+//	var fcdTrips = [FCDTimestep]()
+//	timestepLoop: for timestep in fcdXML["fcd-export"]["timestep"] {
+//		guard let timestepElement = timestep.element,
+//			let s_time = timestepElement.attribute(by: "time")?.text,
+//			let timestepTime = Double(s_time)
+//			else {
+//				throw FloatingCarDataError.invalidTimestamp
+//		}
+//
+//		// Don't load timesteps that occur later than the simulation stopTime
+//		if timestepTime > configStopTime { break timestepLoop }
+//
+//		var timestepVehicles = [FCDVehicle]()
+//		for vehicle in timestep["vehicle"] {
+//			guard let vehicleElement = vehicle.element,
+//				let s_id = vehicleElement.attribute(by: "id")?.text,
+//				let s_xgeo = vehicleElement.attribute(by: "x")?.text,
+//				let s_ygeo = vehicleElement.attribute(by: "y")?.text,
+//				let v_id = UInt(s_id),
+//				let v_xgeo = Double(s_xgeo),
+//				let v_ygeo = Double(s_ygeo)
+//				else {
+//					throw FloatingCarDataError.failedConversion
+//			}
+//			timestepVehicles.append( FCDVehicle(id: v_id, geo: (x: v_xgeo, y: v_ygeo)) )
+//		}
+//		fcdTrips.append( FCDTimestep(time: timestepTime, vehicles: timestepVehicles) )
+//	}
+//	return fcdTrips
+//}
