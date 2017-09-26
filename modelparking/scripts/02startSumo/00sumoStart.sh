e22edac88dedadbfc4bc5f2e6a880a7705d19ecb
Andre Braga Reis <andrebragareis@gmail.com>
2017-09-26 11:29:36 +0100

    Minor corrections pre- topology code

--- modelparking/scripts/02startSumo/00sumoStart.sh
+++ modelparking/scripts/02startSumo/00sumoStart.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ln -sfr ./interact/map_clean3.net.xml /work/map.net.xml
+gunzip ./interact/map_clean3.net.xml.gz
+gunzip ./interact/modules/perSecondArrayNorm.csv.gz
+
 nohup $(dirname $0)/sumoLooper.sh > sumo.log 2>&1 &
